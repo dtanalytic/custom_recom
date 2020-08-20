049ee94bccda83ac31af204915e6d40f3a14da34
dtanalytic <67217088+dtanalytic@users.noreply.github.com>
2020-08-20 15:58:15 +0300

    'sds'

--- recomend.py
+++ recomend.py
@@ -4,10 +4,10 @@ from os import path
 
 if __name__ == '__main__':
     data = np.loadtxt(r'c:\work\dev\python\progs\lessons\data\ml-100k\u.data')
-    ij = data[:, :2]
+    ij = data[:, :2].astype(np.int64)
     ij -= 1  # original data is in 1-based system
     values = data[:, 2]
-    # reviews = sparse.csc_matrix((values, ij.T)).astype(float64)
-    
+    reviews = sparse.csc_matrix((values, ij.T)).astype(float)
+    dsdsds
     # reviews = reviews.toarray()  
 
