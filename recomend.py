47648d954bb96abd9692ac9d428314ea18c36743
al_perscomp <alansozanov@yandex.ru>
2020-08-21 12:07:59 +0400

    новый коммит

--- recomend.py
+++ recomend.py
@@ -1,13 +1,23 @@
 import numpy as np
 from scipy import sparse
 from os import path
+import random
 
 if __name__ == '__main__':
-    data = np.loadtxt(r'c:\work\dev\python\progs\lessons\data\ml-100k\u.data')
-    ij = data[:, :2]
+    data = np.loadtxt(r'../data/ml-100k/u.data')
+    ij = data[:, :2].astype(int)
     ij -= 1  # original data is in 1-based system
     values = data[:, 2]
-    # reviews = sparse.csc_matrix((values, ij.T)).astype(float64)
+    reviews = sparse.csc_matrix((values, ij.T)).astype(float)
     
-    # reviews = reviews.toarray()  
+    reviews = reviews.toarray()  
+    U,M = np.where(reviews)
+    
+    # U,M  = reviews.nonzero()
+    r = random.Random(3)
+    test_idxs = np.array(r.sample(range(len(U)), len(U)//10))
+    train = reviews.copy()
+    train[U[test_idxs], M[test_idxs]] = 0
 
+    test = np.zeros_like(reviews)
+    test[U[test_idxs], M[test_idxs]] = reviews[U[test_idxs], M[test_idxs]]
