e80f513b95f3f671ec8be6391143c79411e324ea
al_perscomp <alansozanov@yandex.ru>
2020-08-24 11:13:41 +0400

    sds

--- recomend.py
+++ recomend.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy import sparse
 from os import path
 import random
+import pandas as pd
 
 if __name__ == '__main__':
 
@@ -10,18 +11,24 @@ if __name__ == '__main__':
     ij -= 1  # original data is in 1-based system
     values = data[:, 2]
     rates = sparse.csc_matrix((values, ij.T)).astype(float)
+    df = pd.DataFrame({'U':ij[:,0],'M':ij[:,1]})
     
     rates = rates.toarray()  
     U,M = np.where(rates)
     
     # U,M  = rates.nonzero()
-    r = random.Random(3)
-    test_idxs = np.array(r.sample(range(len(U)), len(U)//10))
-    train = rates.copy()
-    train[U[test_idxs], M[test_idxs]] = 0
+    # r = random.Random(3)
+    # test_idxs = np.array(r.sample(range(len(U)), len(U)//10))
+    # train = rates.copy()
+    # train[U[test_idxs], M[test_idxs]] = 0
 
 
-    test = np.zeros_like(rates)
-    авава
-    вава
-    test[U[test_idxs], M[test_idxs]] = rates[U[test_idxs], M[test_idxs]]
+    # test = np.zeros_like(rates)
+
+    
+    # test[U[test_idxs], M[test_idxs]] = rates[U[test_idxs], M[test_idxs]]
+    
+    
+    # df = pd.read_csv('../data/titanic.csv')
+    # df.info()
+    # df.shape
